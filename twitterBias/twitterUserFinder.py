234493bcdf210522e9caaaa764cfeceade632673
robaldis <robertaldis@gmail.com>
2020-07-30 22:52:33 +0100

    Starting the twitter user finder script

---
+++ twitterBias/twitterUserFinder.py
@@ -0,0 +1,14 @@
+
+
+def getUser(userList, iterations):
+    newUsers = []
+    for user in userList:
+        # append the new user list to the newUser list
+        break
+
+    userList.append(newUsers)
+    #sort and get rid of duplicates
+    userList.sort()
+    userList = list(dict.fromkeys(userList))
+
+    return userList
